3d6deabec4ea6efb0f78e1d2d6069a5329b4fdd8
cawleygl <grantlcawley@gmail.com>
2020-12-03 15:13:47 -0600

    Added div for first question and added button functionality to display hidden question and hide intro on click

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var startButton = document.querySelector("#start")
+var introPage = document.querySelector("#intro")
+var question1 = document.querySelector("#q1")
+
 
 startButton.addEventListener("click", function() {
     console.log("START!")
+    introPage.classList.add("hide");
+    question1.classList.remove("hide");
 });
