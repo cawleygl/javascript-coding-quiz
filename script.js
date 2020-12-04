6b7e70e0850267fd0d041f4dfcbcb7e7b7531938
cawleygl <grantlcawley@gmail.com>
2020-12-04 17:03:39 -0600

    Added functionality to initials input made initials key for storing scores in local storage, and allowed initials to display on leaderboard

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-var startButton = document.querySelector("#start");
+//Page query selectors
 var introPage = document.querySelector("#intro");
-
 var questionPage1 = document.querySelector("#q1");
 var questionPage2 = document.querySelector("#q2");
 var questionPage3 = document.querySelector("#q3");
 var questionPage4 = document.querySelector("#q4");
 var questionPage5 = document.querySelector("#q5");
-
 var finalPage = document.querySelector("#finalscore");
 var highscorePage = document.querySelector("#highscores");
+
+//Button query selectors
+var startButton = document.querySelector("#start");
 var submitButton = document.querySelector("#submitscore");
 var backButton = document.querySelector("#back");
 var clearButton = document.querySelector("#clear");
+
+//Element query selectors
 var timerEl = document.querySelector("#timer");
 var scoreEl = document.querySelector("#scoredisplay");
 var leaderboardEl = document.querySelector("#leaderboard");
+var initialsEl = document.querySelector("#initials");
 
+//Timer variable reset 
 var timeLeft = 0;
 timerEl.textContent = "Time: " + timeLeft;
 
+//Start button
 startButton.addEventListener("click", function() {
     timeLeft = 75;
     timerEl.textContent = "Time: " + timeLeft;
@@ -31,6 +37,7 @@ startButton.addEventListener("click", function() {
       }, 1000);
 });
 
+//Select an answer for question 1
 questionPage1.addEventListener("click", function(event) {
     event.preventDefault();
     if(event.target.matches("button")) {
@@ -40,7 +47,7 @@ questionPage1.addEventListener("click", function(event) {
     }
   });
 
-
+//Select an answer for question 2
 questionPage2.addEventListener("click", function(event) {
     event.preventDefault();
     if(event.target.matches("button")) {
@@ -50,6 +57,7 @@ questionPage2.addEventListener("click", function(event) {
     }
 });
 
+//Select an answer for question 3
 questionPage3.addEventListener("click", function(event) {
     event.preventDefault();
     if(event.target.matches("button")) {
@@ -59,6 +67,7 @@ questionPage3.addEventListener("click", function(event) {
     }
 });
 
+//Select an answer for question 4
 questionPage4.addEventListener("click", function(event) {
     event.preventDefault();
     if(event.target.matches("button")) {
@@ -68,6 +77,7 @@ questionPage4.addEventListener("click", function(event) {
     }
 });
 
+//Select an answer for question 5
 questionPage5.addEventListener("click", function(event) {
     event.preventDefault();
     if(event.target.matches("button")) {
@@ -77,27 +87,37 @@ questionPage5.addEventListener("click", function(event) {
         clearInterval(interval);
         scoreEl.textContent = "Your score: " + timeLeft + " seconds";
         timerEl.textContent = "Time: 0";
-        localStorage.setItem("lastscore", timeLeft);
         console.log("Time: " + timeLeft);
     }
 });
 
+//Submit button
 submitButton.addEventListener("click", function() {
     console.log("Submit Score")
     finalPage.classList.add("hide");
     highscorePage.classList.remove("hide");
+
     var leaderboardTop = document.createElement("li");
-    var lastScore = localStorage.getItem("lastscore");
-    leaderboardTop.textContent = lastScore;
+    var score = localStorage.getItem("score");
+    var initials = initialsEl.value
+
+    localStorage.setItem(initials, timeLeft);
+
+
+    leaderboardTop.textContent = initials + ": " + score;
     leaderboardEl.appendChild(leaderboardTop);
 
 });
 
+//Go back button
 backButton.addEventListener("click", function() {
     console.log("Go Back")
+    highscorePage.classList.add("hide");
+    introPage.classList.remove("hide");
 
 });
 
+//Clear button
 clearButton.addEventListener("click", function() {
     console.log("Clear Highscores")
 
